3874d3dfc8b53f0a32d694064655b3ffe99890b6
John Doe <johndoe@email.com>
2024-09-24 20:09:13 -0400

    First commit - made changes to Conversation,  reflection and rubric.

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,102 @@
+import java.util.Scanner;
+import java.util.Random;
+
+/**
+ * Class constructor for a simple chat bot conversation. 
+ */
 class Conversation {
 
   public static void main(String[] arguments) {
-    // You will start the conversation here.
+
+    // Canned responses to user input
+    String[] cannedResponses = {
+      "Fascinating.",
+      "I see.",
+      "Hmmm... Tell me more.",
+      "Okay...",
+      "That’s a new perspective."
+    };
+
+    // Create scanner
+    Scanner conversationStart = new Scanner(System.in);
+
+    // Ask user for desired number of rounds
+    System.out.println("How many rounds? ");
+    String numRounds = conversationStart.nextLine();
+
+    // Store desired number of rounds
+    int rounds = Integer.parseInt(numRounds);
+    
+    // Begin conversation
+    String[] transcript = new String[2 * rounds + 1];
+    transcript[0] = "Okie dokey! What's going on today, you?";
+    System.out.println(transcript[0]);
+
+    // Loop for desired number of rounds
+    for(int i = 0; i < Integer.parseInt(numRounds); i++){
+      String userInput = conversationStart.nextLine();
+      String botResponse;
+
+      // Track whether string has been edited
+      boolean edited = false;
+
+      // Split user input
+      String[] splitResponse = userInput.split(" ");
+      for(int j = 0; j < splitResponse.length; j++){
+        String word = splitResponse[j].toLowerCase();
+        String punctuation = "";
+        if (word.matches(".*[.,!?]$")){
+          punctuation = word.substring(word.length() - 1);
+          word = word.substring(0, word.length() - 1);
+        }
+        // Mirror words
+        if (word.equals("i")) {
+          word = "you";
+          edited = true;
+        } else if (word.equals("me")) {
+          word = "you";
+          edited = true;
+        } else if (word.equals("am")) {
+          word = "are";
+          edited = true;
+        } else if (word.equals("your")) {
+          word = "my";
+          edited = true;
+        } else if (word.equals("my")) {
+          word = "your";
+          edited = true;
+        } else if (word.equals("you")) {
+          word = "I";
+          edited = true;
+        } else if (word.equals("are")) {
+          word = "am";
+          edited = true;
+        }
+        splitResponse[j] = word + punctuation;
+      }
+      String joinResponse = String.join(" ", splitResponse);
+      joinResponse = joinResponse.replace(".", "?");
+      String response = joinResponse.substring(0,1).toUpperCase() 
+                        + joinResponse.substring(1);
+      if (edited) {
+        botResponse = response;
+      } else {
+        int randomIndex = new Random().nextInt(cannedResponses.length);
+        botResponse = cannedResponses[randomIndex];
+      }
+      System.out.println(botResponse);
+      transcript[2 * i + 1] = userInput;
+      transcript[2 * i + 2] = botResponse;
+    }
+
+    System.out.println("This has been fun! Let's chat again soon.");
+    System.out.println("\n");
+
+    // Create transcript
+    System.out.println("TRANSCRIPT:");
+    for(int i = 0; i < transcript.length; i++){
+      System.out.println(transcript[i]);
+    }
+    conversationStart.close();
   }
 }
--- reflection.md
+++ reflection.md
@@ -1,7 +1,11 @@
 Use this file to record your reflection on this assignment.
 
 - Which classmates did you consult with while working on this assignment?
+
 - Which session(s) of TA / office hours did you attend?
+
 - What are your initial impressions of Java? 
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+
+My inital impression is that Java is much more strict than Python, for example, requiring a semicolon to be added at the end of each line. Also, more words are required to yield the same result (e.g. "==" in Python is ".equals()" in Java). Despite differences in Python and Java, the main ideas are the same. Each has for and while loops, lists of one form or another, variables, etc. Also, when programming in any language, it can be very helpful to organize your tasks into more managable steps. I found looking through the online textbook to be helpful for understanding syntax and variable types (e.g. arrays). Also, when I was writing, it helped me to think about what I needed to do first, implement that, then add on to what I wrote. I started with an array of canned responses and a scanner to collect user input, and got those working before even thinking about mirror replacements. Also, sometimes it can be helpful to remember that just because something is working doesn't mean you can't change it if you have a better idea.
